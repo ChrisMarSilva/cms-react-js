194f38bcc870940f89547e27f05ccb6d5c1331d2
ChrisMarSilva <chris.mar.silva@gmail.com>
2023-04-19 18:26:52 +0100

    add react with tailwindcss

---
+++ cms-teste-tailwindcss/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div>
+
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+
+      <button class="bg-sky-500 hover:bg-sky-700 ...">
+        Save changes
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
