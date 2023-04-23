6babc43e1a4af26c744b99e5eb8335d6215a1060
ChrisMarSilva <chris.mar.silva@gmail.com>
2023-04-23 23:19:41 +0100

    Update Projects.js

--- cms-teste-react/cms-curso-costs/src/pages/Projects.js
+++ cms-teste-react/cms-curso-costs/src/pages/Projects.js
@@ -19,12 +19,11 @@ export default function Projects() {
     // if (location.state) message = location.state.message
 
     useEffect(() => {
-        // Para ver o loading
         const url = 'http://localhost:5000/projects'
         const headers = { 'Content-Type': 'application/json' }
 
-        // setTimeout(
-        //     () =>
+        // Para ver o loading
+        // setTimeout(() =>
         //         fetch(url, { method: 'GET', headers: headers, })
         //             .then((resp) => resp.json())
         //             .then((data) => {
@@ -40,7 +39,9 @@ export default function Projects() {
             setRemoveLoading(true)
         }
 
-        setTimeout(() => getData(), 100)
+        // Para ver o loading
+        // setTimeout(() => getData(), 100) // Aguarde 100 milisegundos
+        getData()
 
     }, [])
 
@@ -62,17 +63,26 @@ export default function Projects() {
 
     return (
         <div className={styles.project_container}>
+
             <div className={styles.title_container}>
                 <h1>Meus Projetos</h1>
                 <LinkButton to="/newproject" text="Criar projeto" />
             </div>
+
             {message && <Message type="success" msg={message} />}
+
             {projectMessage && <Message type="success" msg={projectMessage} />}
+
             <Container customClass="start">
-                {projects.length > 0 && projects.map((project) => <ProjectCard id={project.id} name={project.name} budget={project.budget} category={project.category.name} key={project.id} handleRemove={removeProject} />)}
                 {!removeLoading && <Loading />}
                 {removeLoading && projects.length === 0 && <p>Não há projetos cadastrados!</p>}
+                {
+                    projects.length > 0
+                    &&
+                    projects.map((project) => <ProjectCard id={project.id} name={project.name} budget={project.budget} category={project.category.name} key={project.id} handleRemove={removeProject} />)
+                }
             </Container>
+
         </div>
     )
 }
